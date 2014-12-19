1f73c0ce5c2944da8b1d6ce8e5210e35726e22ae
Aaron Strick <aaronstrick@gmail.com>
2014-12-19 14:46:40 -0800

    Change module pattern again, add submit button

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,8 +1,12 @@
+(function (root){
+  var ChatApp = root.ChatApp = root.ChatApp || {};
 
+  var Chat = ChatApp.Chat = function(socket) {
+    this.socket = socket;
+  }
 
+  Chat.prototype.sendMessage = function(message) {
+    this.socket.emit("message", {message: message})
+  }
 
-// Use the module pattern to namespace this client-side javascript.
-//
-// In a new file public/javascripts/chat.js, make a class constructor Chat that receives socket as an attribute and stores it for later.
-//
-// Add a sendMessage method to the Chat class for transmitting a message to all users. Use the emit method to emit the message event with the text of the message.
+})(window);
