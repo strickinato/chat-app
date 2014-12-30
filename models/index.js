8e9c691e390369a073e02a553f4c391268cf1529
Aaron Strick <aaronstrick@gmail.com>
2014-12-30 13:49:45 -0800

    Change index.js file to support Heroku

--- models/index.js
+++ models/index.js
@@ -1,31 +1,47 @@
-"use strict";
+if (!global.hasOwnProperty('db')) {
+  var Sequelize = require('sequelize')
+  , sequelize = null
 
-var fs        = require("fs");
-var path      = require("path");
-var Sequelize = require("sequelize");
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || "development";
-var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
-var db        = {};
+  if (process.env.HEROKU_POSTGRESQL_ROSE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    var match = process.env.HEROKU_POSTGRESQL_ROSE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== basename);
-  })
-  .forEach(function(file) {
-    var model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+    sequelize = new Sequelize(match[5], match[1], match[2], {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+  } else {
+    var env       = process.env.NODE_ENV || "development";
+    var config    = require(__dirname + '/../config/config.json')[env];
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
+}
 
-// Object.keys(db).forEach(function(modelName) {
-//   if ("associate" in db[modelName]) {
-//     db[modelName].associate(db);
-//   }
-// });
+    var fs        = require("fs");
+    var path      = require("path");
+    var basename  = path.basename(module.filename);
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+    global.db = {};
 
-module.exports = db;
+
+    fs
+    .readdirSync(__dirname)
+    .filter(function(file) {
+      return (file.indexOf(".") !== 0) && (file !== basename);
+    })
+    .forEach(function(file) {
+      var model = sequelize["import"](path.join(__dirname, file));
+      global.db[model.name] = model;
+    });
+
+
+    global.db = {
+      Sequelize: Sequelize,
+      sequelize: sequelize
+    }
+    console.log(global.db.sequelize.models)
+
+module.exports = global.db
