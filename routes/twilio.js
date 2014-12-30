ef6528ff120b662fc755d9e7ae3d51f8cd6f67f9
Aaron Strick <aaronstrick@gmail.com>
2014-12-30 15:23:51 -0800

    fix model require in twilio.js

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var models = require('models')
+var models = require('../models')
 var twilio = require('twilio');
 var router  = express.Router();
 var AARON = '+14152720970'
