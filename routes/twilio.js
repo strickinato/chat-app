7ccc7310a25e1eaf6b107b18bdcc59f750e34c80
Aaron Strick <aaronstrick@gmail.com>
2014-12-30 15:08:37 -0800

    Check req params

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var twilio = require('twilio');
 var router  = express.Router();
+var AARON = '+14152720970'
 
 router.post('/', function(req, res) {
-  // Create a TwiML res
+  if(req.body.From == AARON) {
+    var twiml = new twilio.TwimlResponse();
+    twiml.message('Hello from node.js!');
 
-  
-  var twiml = new twilio.TwimlResponse();
-  twiml.message('Hello from node.js!');
+    // Render the TwiML res as XML
+    res.type('text/xml');
+    res.send(twiml.toString());
+  }
 
-  // Render the TwiML res as XML
-  res.type('text/xml');
-  res.send(twiml.toString());
 });
 
 
