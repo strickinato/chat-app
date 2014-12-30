8d71c84a4b2c08b203cbd1f87c3a39f8b7face6d
Aaron Strick <aaronstrick@gmail.com>
2014-12-30 15:12:24 -0800

    Find out about param hunt

--- routes/chat.js
+++ routes/chat.js
@@ -7,7 +7,8 @@ var router = express.Router();
 // route middleware that will happen on every request
 router.use(function(req, res, next) {
   console.log(req.method, req.url);
-  console.log(req.params)
+  console.log("req.params" + req.body.From)
+  console.log("req.param('From')" requ.param('From'))
   next();
 });
 
