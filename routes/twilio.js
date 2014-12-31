3b5defb54a55127e12639b1132d943bf77d1e847
Aaron Strick <aaronstrick@gmail.com>
2014-12-31 02:38:47 -0800

    pass io as param to twilio

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,8 +1,10 @@
-var models = require('../models')
-var AARON = '+14152720970'
+var models = require('../models');
+var express = require('express')
+var router = express.Router();
+var AARON = '+14152720970';
 
-module.exports = function(app, io) {
-  app.post('/', function(req, res) {
+module.exports = function(io) {
+  twilioRouter = function(req, res) {
     if(req.param('From') == AARON) {
       var newMessage = models.Message.build({
         body: req.param('Body'),
@@ -12,5 +14,6 @@ module.exports = function(app, io) {
         io.emit('message', newMessage.body)
       })
     }
-  })
+  }
+  return twilioRouter;
 }
