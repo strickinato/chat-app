c89cab5d2ed771c82c40c49218550095652d83dd
Aaron Strick <aaronstrick@gmail.com>
2014-12-31 01:44:34 -0800

    Changed twilio router export

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,22 +1,16 @@
-var express = require('express');
 var models = require('../models')
-var twilio = require('twilio');
-var socket = require('socket.io-client');
-var router  = express.Router();
 var AARON = '+14152720970'
 
-router.post('/', function(req, res) {
-  if(req.param('From') == AARON) {
-    var newMessage = models.Message.build({
-      body: req.param('Body'),
-      aaron: true
-    })
-    newMessage.save().then(function(){
-      socket.emit('message', newMessage.body);
-    })
-  }
-
-});
-
-
-module.exports = router;
+module.exports = function(app, io) {
+  app.post('/', function(req, res) {
+    if(req.param('From') == AARON) {
+      var newMessage = models.Message.build({
+        body: req.param('Body'),
+        aaron: true
+      })
+      newMessage.save().then(function(){
+        io.emit('message', newMessage.body)
+      })
+    }
+  })
+}
